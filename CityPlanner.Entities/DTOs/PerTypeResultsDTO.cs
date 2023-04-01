b69f9ed37c319b07d224f2fde3746a8180f123c7
dkorecko <22599403+rese-sw@users.noreply.github.com>
2023-04-01 22:01:29 +0200

    added score calculation

---
+++ CityPlanner.Entities/DTOs/PerTypeResultsDTO.cs
@@ -0,0 +1,15 @@
+﻿using CityPlanner.Entities.Entities;
+
+namespace CityPlanner.Entities.DTOs
+{
+    public class PerTypeResultsDTO
+    {
+        public int TotalCount { get; set; }
+
+        public double? ClosestDistance { get; set; }
+
+        public InterestPoint? ClosestPoint { get; set; }
+
+        public List<InterestPoint> PointsByClosest { get; set; } = new();
+    }
+}
---
+++ CityPlanner.Entities/DTOs/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace CityPlanner.Entities.DTOs
+{
+    public class UserDTO
+    {
+        public bool WantsRestaurant { get; set; }
+
+        public bool WantsBar { get; set; }
+
+        public bool WantsPub { get; set; }
+
+        public bool WantsCafe { get; set; }
+
+        public bool WantsFastFood { get; set; }
+
+        public bool WantsDogEnclosure { get; set; }
+
+        public bool WantsGym { get; set; }
+
+        public bool WantsOutsideGym { get; set; }
+    }
+}
