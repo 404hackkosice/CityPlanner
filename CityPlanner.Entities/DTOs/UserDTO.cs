7391a9ebf9a986845b4b579be5be91fb0dcd6cee
Samuel Neceda <samuel.neceda@gmail.com>
2023-04-01 20:39:29 +0200

    ranking incomplete

---
+++ CityPlanner.Entities/DTOs/UserDTO.cs
@@ -0,0 +1,33 @@
+namespace CityPlanner.Entities.DTOs
+{
+    public class UserDTO
+    {
+        public bool PostOffice { get; set; }
+        public bool Chemist { get; set; }
+        public bool PublicTransportStop { get; set; }
+        public bool Pharmacy { get; set; }
+        public bool Supermarket { get; set; }
+        public bool GeneralClinicForAdults { get; set; }
+        public bool DentalClinic { get; set; }
+        public bool ParcelLocker { get; set; }
+        public bool Convenience { get; set; }
+        public bool ChildrenAllowed { get; set; }
+        public bool ChildrenWithinFiveYearsAllowed { get; set; }
+        public bool ChildrenNeverAllowed { get; set; }
+        public bool PublicElementarySchool { get; set; }
+        public bool ChurchElementarySchool { get; set; }
+        public bool PrivateElementarySchool { get; set; }
+        public bool PublicKindergarten { get; set; }
+        public bool PrivateKindergarten { get; set; }
+        public bool ChurchKindergarten { get; set; }
+        public bool GeneralClinicForChildren { get; set; }
+        public bool Playground { get; set; }
+        public bool Restaurant { get; set; }
+        public bool Bar { get; set; }
+        public bool Pub { get; set; }
+        public bool FastFood { get; set; }
+        public bool Cafe { get; set; }
+        public bool DogEnclosures { get; set; }
+        public bool FitnessStation { get; set; }
+    }
+}
