40be7f88c8076c78fcd261433923acdf79063c7a
reeg <andrej.kazmirsky@student.tuke.sk>
2023-04-01 13:37:18 +0200

    added Buildings & Bus stops

---
+++ CityPlanner.Entities/Entities/Building.cs
@@ -0,0 +1,19 @@
+﻿namespace CityPlanner.Entities.Entities
+{
+    public class Building
+    {
+        public int Id { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string Street { get; set; } = null!;
+        public string OrientationNumber { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public string District { get; set; } = null!;
+
+        public int HousingCount { get; set; }
+    }
+}
---
+++ CityPlanner.Entities/Entities/BusStop.cs
@@ -0,0 +1,23 @@
+﻿namespace CityPlanner.Entities.Entities
+{
+    public class BusStop
+    {
+        public int Id { get; set; }
+
+        public int Pasport { get; set; }
+
+        public int Sl { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string Direction { get; set; } = null!;
+
+        public string District { get; set; } = null!;
+
+        public Boolean Tram { get; set; }
+        public Boolean Trolley { get; set; }
+        public Boolean Bus { get; set; }
+    }
+}
