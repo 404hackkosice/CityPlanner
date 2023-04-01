07a4064eabd895ec42765b748b88f59429b9146d
reeg <andrej.kazmirsky@student.tuke.sk>
2023-04-01 15:41:03 +0200

    remove bootstrap & loading spinner

--- CityPlanner.Web/tailwind.config.js
+++ CityPlanner.Web/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./**/*.{razor,html,cshtml}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(100%)' },
+        },
+        slideOut: {
+          '0%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 1s ease-in-out',
+        slideOut: 'slideOut 1s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
