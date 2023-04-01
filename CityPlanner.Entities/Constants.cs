fd157e3e15216be617c20dda5c657bcd0ffaf854
dkorecko <22599403+rese-sw@users.noreply.github.com>
2023-04-02 00:48:03 +0200

    added interest point types

--- CityPlanner.Entities/Constants.cs
+++ CityPlanner.Entities/Constants.cs
@@ -13,6 +13,28 @@
             public const string ADDRESSES = "addresses.csv";
         }
 
+        public static class PointTypes
+        {
+            public const string POST_OFFICE = "Pošty";
+            public const string DENTAL_CLINIC = "ambulancia zubného lekárstva";
+            public const string GENERAL_CLINIC_ADULTS = "Všeobecná ambulancia pre dospelých";
+            public const string SUPERMARKET = "supermarket";
+            public const string CHEMIST = "chemist";
+            public const string PHARMACY = "pharmacy";
+            public const string CONVENIENCE = "convenience";
+            public const string PARCEL_LOCKER = "parcel_locker";
+            public const string PUBLIC_TRANSPORT = "MHD";
+            public const string OUTSIDE_GYM = "fitness_station";
+            public const string GYM = "fitness_fitness";
+            public const string RESTAURANT = "restaurant";
+            public const string BAR = "bar";
+            public const string PUB = "pub";
+            public const string CAFE = "cafe";
+            public const string FAST_FOOD = "fast_food";
+            public const string DOG_ENCLOSURE = "Vybehy_psy";
+
+        }
+
 
         public static readonly string WWW_ROOT = $"{AppDomain.CurrentDomain.BaseDirectory}/wwwroot";
         public static readonly string DATA_PATH = $"{WWW_ROOT}/data";
