ad8f50de7aa77802539b3e19ffe03ecca3534d5a
reeg <andrej.kazmirsky@student.tuke.sk>
2023-04-01 18:18:20 +0200

    up tailwind

--- CityPlanner.Web/tailwind.config.js
+++ CityPlanner.Web/tailwind.config.js
@@ -1,22 +1,9 @@
 module.exports = {
   content: ["./**/*.{razor,html,cshtml}"],
   theme: {
-    extend: {
-      keyframes: {
-        slideIn: {
-          '0%': { transform: 'translateY(0%)' },
-          '100%': { transform: 'translateY(100%)' },
-        },
-        slideOut: {
-          '0%': { transform: 'translateY(0%)' },
-          '100%': { transform: 'translateY(-100%)' },
-        },
-      },
-      animation: {
-        slideIn: 'slideIn 1s ease-in-out',
-        slideOut: 'slideOut 1s ease-in-out',
-      }
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate")
+  ],
 }
