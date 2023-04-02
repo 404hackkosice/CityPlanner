93576bc621d4609c3e1f3e0704492bdad0e8d413
reeg <andrej.kazmirsky@student.tuke.sk>
2023-04-02 11:03:44 +0200

    fix text overflow

--- CityPlanner.Web/wwwroot/js/main.js
+++ CityPlanner.Web/wwwroot/js/main.js
@@ -3,14 +3,14 @@ function moveCarousel(direction) {
     if (direction == 'left') {
         el.scrollBy({
             top: 0,
-            left: -250,
+            left: -350,
             behavior: 'smooth'
         });
     }
     else {
         el.scrollBy({
             top: 0,
-            left: +250,
+            left: +350,
             behavior: 'smooth'
         })
     }
